69f9f7102f646ca9495d76235ce251f78dddd84e
Danny Hsu <danny539425@gmail.com>
2023-07-06 23:49:53 +0800

    add img

--- commands/ping.js
+++ commands/ping.js
@@ -6,6 +6,9 @@ module.exports = {
         const embed = new EmbedBuilder()
         .setTitle('I\'m an Embed')
         .setColor("Random")
+        .setImage("https://imgur.com/a/sB7a4gY")
+        // .attachFiles([{name: '004.gif', attachment: 'file:///C:/Users/%E8%A8%B1%E5%AE%B8%E7%A0%94/Documents/GitHub/discord-bot666/004.gif'}])
+        // .setImage("attachment://004.gif")
         .addFields({ name: 'Inline field title', value: 'Some value here', inline: true });
         await interaction.reply({ embeds: [embed] });
     },
